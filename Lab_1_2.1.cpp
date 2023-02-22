c0c284d6c69c6bb81fd9808ced2619fdda61b5d7
Paul <126007486+Hatakira@users.noreply.github.com>
2023-02-21 21:58:31 -0500

    Create Lab_1_2.1.cpp
    Basic Hello World using "const char*" in Lab 1 2.1

---
+++ Lab_1_2.1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Write a program that outputs “Hello, World!” by printing a const char * with value
+“Hello, World!”. 
+*/
+
+int main(){
+
+    const char*text = "Hello,World!";
+
+    cout<<text;
+
+    return 0 ;
+}
